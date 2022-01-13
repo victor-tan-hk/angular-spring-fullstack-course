e796220be9371c66c1e5e11c67b78d9412b20278
Peter Parker <spiderman@gmail.com>
2022-01-13 14:53:53 +0800

    correct file error in Angular Lab 1

--- 2 - Angular/lab-code/Binding-Demo/changes/app.module-v9.ts
+++ 2 - Angular/lab-code/Binding-Demo/changes/app.module-v9.ts
@@ -1,30 +1,18 @@
-<h1> My first Angular app </h1>
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-<!-- Basic form of two way binding -->
-<label for="firstbox">First text box:</label><br>
-<input #firstbox type="text" id="firstbox" name="firstbox" [value]="firstBoxContent" (input)="firstBoxContent = firstbox.value">
+import { AppComponent } from './app.component';
 
-<p>Value of firstBoxContent : {{firstBoxContent}}</p>
-
-<!-- using ngModel and ngModelChange to perform event and property binding -->
-<label for="secondbox">Second text box:</label><br>
-<input type="text" id="secondbox" name="secondbox" [ngModel]="secondBoxContent" (ngModelChange)="secondBoxContent=$event">
-
-<p>Value of secondBoxContent : {{secondBoxContent}}</p>
-
-<!-- Standard banana-in-the-box syntax for ngModel  -->
-<label for="thirdbox">Third text box:</label><br>
-<input type="text" id="thirdbox" name="thirdbox" [(ngModel)]="thirdBoxContent">
-
-<p>Value of thirdBoxContent : {{thirdBoxContent}}</p>
-
-
-<label for="fourthbox">Fourth text box:</label><br>
-<input  type="text" id="fourthbox" name="fourthbox" [ngModel]="fourthBoxContent" (ngModelChange)="processFourthBox($event)">
-
-<p>Value of fourthBoxContent : {{fourthBoxContent}}</p>
-
-
-<button type="button"(click)="resetTextBoxes()">Reset content in all boxes to: {{initialValue}}</button>
-
-
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
